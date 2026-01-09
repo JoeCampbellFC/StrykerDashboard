c7b92bd566828a3ed7d2be0a8d86065a15e9c75a
Joe Campbell <144352985+JoeCampbellFC@users.noreply.github.com>
2026-01-09 16:55:52 +0000

    Update documents-table.tsx

--- components/dashboard/documents-table.tsx
+++ components/dashboard/documents-table.tsx
@@ -192,7 +192,7 @@ export function DocumentsTable({
                     <TableHead>Title</TableHead>
                     <TableHead className="whitespace-nowrap">Date</TableHead>
                     <TableHead>Folder</TableHead>
-                    <TableHead>File</TableHead>
+                    <TableHead></TableHead>
                   </TableRow>
                 </TableHeader>
                 <TableBody>
